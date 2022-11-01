f71d41be894002cd56de67f2ce2be90a8dd98397
Parth <94388365+praxton74@users.noreply.github.com>
2022-11-01 12:04:06 +0530

    Create program.cpp

---
+++ swap_nodes_in_LL/program.cpp
@@ -0,0 +1,13 @@
+ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+        while (--k)
+            ptr1 = ptr1->next;
+        
+        kth = ptr1;
+        ptr1 = ptr1->next;
+        
+        while (ptr1) {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        swap(ptr2->val, kth->val);
+        return head;
