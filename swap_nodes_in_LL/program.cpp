0e64c48b4d69f05bbcab1d2e70cdd2b29fc5d19c
Parth <94388365+praxton74@users.noreply.github.com>
2022-11-01 12:04:28 +0530

    Update program.cpp

--- swap_nodes_in_LL/program.cpp
+++ swap_nodes_in_LL/program.cpp
@@ -1,4 +1,18 @@
-ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k)
+    {
+        ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
         while (--k)
             ptr1 = ptr1->next;
         
@@ -11,3 +25,5 @@ ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
         }
         swap(ptr2->val, kth->val);
         return head;
+    }
+};
