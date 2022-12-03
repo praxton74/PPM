023666e1d22810ba9b77ce08091a20c706a25357
Parth <94388365+praxton74@users.noreply.github.com>
2022-12-03 23:28:44 +0530

    Good-Revise

---
+++ Minimum Cost Valid String/program.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+#include<stack>
+int findMinimumCost(string str) {
+ // Write your code here
+   
+   if(str.length()%2==1){
+       return -1;
+   }
+   stack<char>s;
+   for(int i=0; i<str.length();i++){
+       char ch = str[i];
+   
+   if(ch=='{')
+       s.push(ch);
+   else{
+       //ch is closed brace
+       if(!s.empty() && s.top()=='{'){
+           s.pop();
+       }
+       else{
+           s.push(ch);
+       }
+   }
+   }  
+   // stack contain invalid expression
+   int a=0,  b=0;
+   while(!s.empty()){
+       if(s.top()=='{'){
+           b++;
+       }
+       else{
+           a++;
+       }
+       s.pop();
+   }
+   int ans = (a+1)/2 + (b+1)/2;
+   return ans;
+   
+   
+}
