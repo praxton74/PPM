facfa4b7a008cc78b305fe9b7c3bc0d68daeb978
Parth <94388365+praxton74@users.noreply.github.com>
2022-12-16 21:23:18 +0530

    Create program.cpp

---
+++ BottomView Tree/program.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+    vector<int> BottomView(Node *root)
+    {
+        //Your code here
+        
+        vector<int>ans;
+        if(root==NULL)
+        {
+            return ans;
+        }
+        
+        queue<pair<Node*,int>>q;
+        map<int,int>mp;
+        q.push({root,0});
+        
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            
+            Node*node=it.first;
+            int line=it.second;
+            
+            //storing in map
+            
+            mp[line]=node->data;
+            
+            if(node->left!=NULL)
+            {
+                q.push({node->left,line-1});
+            }
+            
+            if(node->right!=NULL)
+            {
+                q.push({node->right,line+1});
+            }
+            
+        }
+        
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+
+};
