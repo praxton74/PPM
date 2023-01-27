f4272da165c9b8349a088ee2b6bdbe800b456276
Parth <94388365+praxton74@users.noreply.github.com>
2023-01-28 01:23:55 +0530

    Flipkart

---
+++ Perfect Peak Array/program.cpp
@@ -0,0 +1,23 @@
+int Solution::perfectPeak(vector<int> &A) 
+{
+    std::unordered_set<int> set;
+    int n = A.size();
+    int maxSoFar = A[0];
+    int minSoFar = A[n-1];
+
+    for(int i=1;i<n-1;i++){
+        if(A[i] > maxSoFar){
+            set.insert(i);
+            maxSoFar = A[i];
+        }
+    }
+    for(int i=n-1;i>0;i--){
+        if(A[i] < minSoFar){
+            minSoFar = A[i];
+            if(set.find(i) != set.end()) 
+            return 1;
+        }
+    }
+    return 0;
+  
+}
