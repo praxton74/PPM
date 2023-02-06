cb0e8655bf9a969b98451195c478ed5f8967d46c
Parth <94388365+praxton74@users.noreply.github.com>
2023-02-06 15:47:22 +0530

    Create program.cpp

---
+++ Shufle Array/program.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n)
+    {
+        if(nums.size()==1)
+        {
+            return nums;
+        }    
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
+        }
+        return ans;
+    }
+};
