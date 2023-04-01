0a35a741545e07d1d7e63072ec86518e0d239c66
Parth <94388365+praxton74@users.noreply.github.com>
2023-04-01 22:50:08 +0530

    ****************************

---
+++ TREE_REVISION/Sum of nodes on the longest path from root to leaf node .cpp
@@ -0,0 +1,35 @@
+void solve(Node* root,int sum, int &maxsum,int l,int &maxlen)
+   {
+       if(root==NULL)
+       {
+           if(l>maxlen)
+           {
+               maxsum=sum;
+               maxlen=l;
+           }
+           else if(l==maxlen)
+           {
+               maxsum=max(sum,maxsum);
+           }
+           return;
+       }
+       
+       sum=sum+root->data;
+       
+       solve(root->left,sum,maxsum,l+1,maxlen);
+       
+       solve(root->right,sum,maxsum,l+1,maxlen);
+       
+   }
+
+    public:
+    int sumOfLongRootToLeafPath(Node* root)
+    {
+        //code here
+        int maxsum=INT_MIN;
+        int l=0;
+        int sum=0;
+        int maxlen=0;
+        solve(root,sum,maxsum,l,maxlen);
+        return maxsum;
+    }
